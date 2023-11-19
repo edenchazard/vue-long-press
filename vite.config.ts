61a081c8ac5058b52431916cbb2604220cb05fd6
Charley Reeson <neon.fortytwo@gmail.com>
2023-11-19 18:16:14 +0000

    fix pkg

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig(({ mode }) => {
         outDir: './dist',
         emptyOutDir: true,
         lib: {
-          entry: resolve(__dirname, 'src/index.ts'),
+          entry: resolve(__dirname, 'src/vue-slick-press/index.ts'),
           name: 'VueSlickPress',
           formats: ['es', 'cjs'],
         },
