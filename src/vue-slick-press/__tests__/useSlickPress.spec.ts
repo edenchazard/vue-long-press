b393d630991d30a2b6d803e582cf4ebdf08610a9
Charley Reeson <neon.fortytwo@gmail.com>
2023-11-19 18:06:49 +0000

    dummy tests

---
+++ src/vue-slick-press/__tests__/useSlickPress.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+import { useSlickPress } from '../index';
+
+describe('useSlickPress', () => {
+  it('dummy', () => {
+    expect('needs testing').toBe('needs testing');
+  });
+});
